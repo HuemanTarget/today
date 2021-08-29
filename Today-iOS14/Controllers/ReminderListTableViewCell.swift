40fe901ebb890a53e2a1a157b34b143e1c8711cd
Joshua Basche <joshbasche@live.com>
2021-08-29 14:36:55 -0700

    add testData to Reminder model

--- Today-iOS14/Controllers/ReminderListTableViewCell.swift
+++ Today-iOS14/Controllers/ReminderListTableViewCell.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class ReminderListTableViewCell: UITableViewCell {
-
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var dateLabel: UILabel!
+  
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
