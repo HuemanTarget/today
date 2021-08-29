05ded3cc83e02223048d7a7576573068d1905976
Joshua Basche <joshbasche@live.com>
2021-08-29 14:57:42 -0700

    change ViewController to ReminderListViewController

--- Today-iOS14/Controllers/ReminderListTableViewCell.swift
+++ Today-iOS14/Controllers/ReminderListTableViewCell.swift
@@ -8,18 +8,16 @@
 import UIKit
 
 class ReminderListTableViewCell: UITableViewCell {
+  typealias DoneButtonAction = () -> Void
+  
+  // MARK: - IBOutlets
   @IBOutlet weak var titleLabel: UILabel!
   @IBOutlet weak var dateLabel: UILabel!
+  @IBOutlet weak var doneButton: UIButton!
   
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+  var doneButtonAction: DoneButtonAction?
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+  @IBAction func doneButtonTriggered(_ sender: UIButton) {
+    doneButtonAction?()
+  }
 }
