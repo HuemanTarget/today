d57416b57f38eed4aa9ee14c2f8ce58c3a9dd104
Joshua Basche <joshbasche@live.com>
2021-08-29 15:02:24 -0700

    create ReminderDetailViewController

---
+++ Today-iOS14/Controllers/ReminderDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ReminderDetailViewController.swift
+//  Today-iOS14
+//
+//  Created by Joshua Basche on 8/29/21.
+//
+
+import UIKit
+
+class ReminderDetailViewController: UITableViewController {
+  
+  enum ReminderRow: Int, CaseIterable {
+    case title
+    case date
+    case notes
+    case time
+  }
+  
+}
