a3dc9d09ee585c88bf481e7aaa04ce15d01a236f
Joshua Basche <joshbasche@live.com>
2021-08-29 15:10:50 -0700

    create ReminderRow enum in ReminderDetailViewController

--- Today-iOS14/Controllers/ReminderDetailViewController.swift
+++ Today-iOS14/Controllers/ReminderDetailViewController.swift
@@ -14,6 +14,38 @@ class ReminderDetailViewController: UITableViewController {
     case date
     case notes
     case time
+    
+    func displayText(for reminder: Reminder?) -> String? {
+      switch self {
+      case .title:
+        return reminder?.title
+      case .date:
+        return reminder?.dueDate.description
+      case .notes:
+        return reminder?.notes
+      case .time:
+        return reminder?.dueDate.description
+      }
+    }
+    
+    var cellImage: UIImage? {
+      switch self {
+      case .title:
+        return nil
+      case .date:
+        return UIImage(systemName: "calendar.circle")
+      case .notes:
+        return UIImage(systemName: "sqaure.and.pencil")
+      case .time:
+        return UIImage(systemName: "clock")
+      }
+    }
+  }
+  
+  var reminder: Reminder?
+  
+  func configure(with reminder: Reminder) {
+    self.reminder = reminder
   }
   
 }
