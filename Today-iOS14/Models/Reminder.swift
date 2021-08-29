4158cd10adaa7f82c8febc68a8d5d7871a526ae8
Joshua Basche <joshbasche@live.com>
2021-08-29 14:23:58 -0700

    create Reminder model

---
+++ Today-iOS14/Models/Reminder.swift
@@ -0,0 +1,15 @@
+//
+//  Reminder.swift
+//  Today-iOS14
+//
+//  Created by Joshua Basche on 8/29/21.
+//
+
+import Foundation
+
+struct Reminder {
+  var title: String
+  var dueDate: Date
+  var isCompleted: Bool
+}
+
