d6174c08a5682a28fb6014cdbea3f6558be72dd1
Joshua Basche <joshbasche@live.com>
2021-08-29 14:33:03 -0700

    create ReminderListTableViewCell

---
+++ Today-iOS14/Controllers/ReminderListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ReminderListTableViewCell.swift
+//  Today-iOS14
+//
+//  Created by Joshua Basche on 8/29/21.
+//
+
+import UIKit
+
+class ReminderListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
